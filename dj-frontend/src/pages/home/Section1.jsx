8dbbf3c04fa71e37c1c9c1060e8c1c0c0482c54a
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-06-25 13:32:47 -0300

    styles(home): add hero in section 1

--- dj-frontend/src/pages/home/Section1.jsx
+++ dj-frontend/src/pages/home/Section1.jsx
@@ -1,10 +1,11 @@
-// import SectionContainer from "src/components/SectionContainer";
+import Hero  from "../../assets/images/hero.svg?react";
+import '../styles/Section1.css'
 
 import SectionContainer from "src/components/SectionContainer";
 
 export default function Section1() {
 
-    return <SectionContainer>
-
+    return <SectionContainer style={{backgroundColor: "#E3AE79"}}>
+            <Hero style={{width:"65%"}}/>
     </SectionContainer>
 }
