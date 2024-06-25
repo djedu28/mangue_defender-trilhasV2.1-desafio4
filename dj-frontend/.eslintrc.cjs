9369745d4b3956f97fe4147709be43bf661f3ba5
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2024-06-25 06:58:36 -0300

    ajustando configurações para ativar o intellisense

--- dj-frontend/.eslintrc.cjs
+++ dj-frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, node: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -8,14 +8,35 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: { 
+    ecmaVersion: 'latest', sourceType: 'module',
+    "import/resolver": {
+      alias: {
+        map: [
+          ["src", "./src"]
+        ],
+        extensions: [
+          ".js",
+          ".jsx",
+          ".json"
+        ]
+      }
+    }
+  },
+  settings: { 
+    react: { version: 'detect' }
+  },
+  plugins: [
+    "react",
+    "react-hooks",
+    "react-refresh",
+  ],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off",
   },
 }
