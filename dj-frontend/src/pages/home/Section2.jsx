3dc53df3a5693a56d12d00f729418884f0f9fe78
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-06-25 18:07:49 -0300

    feat: add section 2

--- dj-frontend/src/pages/home/Section2.jsx
+++ dj-frontend/src/pages/home/Section2.jsx
@@ -1,10 +1,26 @@
-// import SectionContainer from "src/components/SectionContainer";
-
+import Roots from "src/assets/images/roots.svg?react";
 import SectionContainer from "src/components/SectionContainer";
-
+import Plate from "src/assets/images/plate.svg?react";
+import About from "src/assets/images/about.svg?react";
+import "../styles/Section2.css";
 export default function Section2() {
-
-    return <SectionContainer>
-
+  return (
+    <SectionContainer style={{ backgroundColor: "#91FE96" }}>
+      <Roots
+        style={{
+          position: "absolute",
+          top: "-70px",
+          left: "0",
+          width: "70%",
+          zIndex: "1",
+        }}
+      />
+      <div className="content">
+        <div id="image_about">
+          <Plate style={{ width: "100%", height: "70%" }} />
+        </div>
+        <About style={{width: "100%", height: "100%", zIndex:'2'}}/>
+      </div>
     </SectionContainer>
+  );
 }
