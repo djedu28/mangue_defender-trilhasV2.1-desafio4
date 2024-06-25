aa1cca6650eac31a34e40cfcbf16c3ed228b3e91
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2024-06-25 07:41:32 -0300

    adicionando as fontes usadas no projeto

--- dj-frontend/src/main.jsx
+++ dj-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './main.css'
+import 'src/assets/font-face.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
